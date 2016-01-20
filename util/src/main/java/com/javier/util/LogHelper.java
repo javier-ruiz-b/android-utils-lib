1c9c8ee4a96381097f2bf9f6d9c27a0b310f3f08
Javier <jr@micromovie.com>
2016-01-20 18:28:36 +0100

    [add] logException in LogHelper.java

--- util/src/main/java/com/javier/util/LogHelper.java
+++ util/src/main/java/com/javier/util/LogHelper.java
@@ -6,7 +6,6 @@ import android.util.Log;
  * Created by javier on 04.01.16.
  */
 public class LogHelper {
-
     /**
      * Enables/disables all logging messages
      */
@@ -22,7 +21,10 @@ public class LogHelper {
      */
     private static final int STACK_TRACE_LEVELS_UP = 5;// TODO: magic number
 
-    //TODO: add warning and error methods
+    private static String getAppIdLogTag() {
+        final int MAX_ALLOWED_TAG_CHARS = 22;
+        return LOG_TAG.substring(0, MAX_ALLOWED_TAG_CHARS); // max chars allowed
+    }
 
     /**
      * Log event by using the default tag {@link LogHelper#LOG_TAG}
@@ -30,7 +32,7 @@ public class LogHelper {
      * @param message
      */
     public static void logEvent(String message) {
-        logEvent(LOG_TAG, message);
+        logEvent(getAppIdLogTag(), message);
     }
 
     /**
@@ -45,6 +47,23 @@ public class LogHelper {
         }
     }
 
+    public static void logException(String logTag, String message, Throwable e) {
+        if (LOGGING_ENABLED) {
+            Log.e(logTag, getClassNameMethodNameAndLineNumber() + message, e);
+        }
+    }
+
+    public static void logException(String message, Throwable e) {
+        if (LOGGING_ENABLED) {
+            Log.e(getAppIdLogTag(), getClassNameMethodNameAndLineNumber() + message, e);
+        }
+    }
+
+    public static void logException(Throwable e) {
+        if (LOGGING_ENABLED) {
+            Log.e(getAppIdLogTag(), getClassNameMethodNameAndLineNumber(), e);
+        }
+    }
 
     /**
      * Gets class name of the given stack trace element without the .java suffix
@@ -55,7 +74,7 @@ public class LogHelper {
         String fileName = element.getFileName();
 
         // Removing ".java" and returning class name
-        return fileName.substring(0, fileName.length() - 5);
+        return fileName.substring(0, fileName.length() - ".java".length());
     }
 
     /**
